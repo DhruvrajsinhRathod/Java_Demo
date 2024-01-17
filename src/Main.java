4c2864b28555c7f4004272563829fa5a6a7467a5
Dhruv <ddrathod510@gmail.com>
2024-01-17 13:34:26 -0500

    average of 5 num counter

--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,19 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-//        p = 2πr
-//        A = π r²
-
         Scanner in = new Scanner(System.in);
-        System.out.print("Input radius : ");
-        double radius = in.nextDouble();
-        double p = 2 * Math.PI * radius;
-        double a = Math.PI * radius * radius;
-
-        System.out.println("The perimeter is : " + p);
-        System.out.println("The area is : " +a);
+        System.out.print("Input num1 : ");
+        int num1 = in.nextInt();
+        System.out.print("Input num2 : ");
+        int num2 = in.nextInt();
+        System.out.print("Input num3 : ");
+        int num3 = in.nextInt();
+        System.out.print("Input num4 : ");
+        int num4 = in.nextInt();
+        System.out.print("Input num5 : ");
+        int num5 = in.nextInt();
+        int total = num1+num2+num3+num4+num5;
+        int average = total/5;
+        System.out.println("The average is: "+average);
         }
     }
