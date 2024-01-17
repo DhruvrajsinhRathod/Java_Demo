8bb0dac3e96bd16853a897d97dcbcd9e58be0bee
Dhruv <ddrathod510@gmail.com>
2024-01-17 13:17:48 -0500

    JAVA pattern

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,9 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        System.out.print("Input the number: ");
-        int num1 = in.nextInt();
-        for(int i=1;i<=10;i++){
-            int total = num1 * i;
-            System.out.println(num1 + "*" + i + "=" + total);
-        }
+        System.out.println("   J    a   v     v  a ");
+        System.out.println("   J   a a   v   v  a a");
+        System.out.println("J  J  aaaaa   V V  aaaaa");
+        System.out.println(" JJ  a     a   V  a     a");
     }
 }
