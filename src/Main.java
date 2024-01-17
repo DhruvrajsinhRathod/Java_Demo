bb0bc31a231da3b942d909f8586ca00aa6773804
Dhruv <ddrathod510@gmail.com>
2024-01-17 13:16:36 -0500

    added multiplication table

--- src/Main.java
+++ src/Main.java
@@ -3,14 +3,11 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        System.out.print("Input the first number: ");
+        System.out.print("Input the number: ");
         int num1 = in.nextInt();
-        System.out.print("Input the second number: ");
-        int num2 = in.nextInt();
-        System.out.println(num1 + "+" + num2 + "=" + (num1+num2));
-        System.out.println(num1 + "-" + num2 + "=" + (num1-num2));
-        System.out.println(num1 + "*" + num2 + "=" + (num1*num2));
-        System.out.println(num1 + "/" + num2 + "=" + (num1/num2));
-        System.out.println(num1 + "%" + num2 + "=" + (num1%num2));
+        for(int i=1;i<=10;i++){
+            int total = num1 * i;
+            System.out.println(num1 + "*" + i + "=" + total);
+        }
     }
 }
