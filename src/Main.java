cdf7e4f43a7865d5cc2345d365d81409d9f601ad
Dhruv <ddrathod510@gmail.com>
2024-01-18 21:43:11 -0500

    Clear Working Directory

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,5 @@
-import java.util.Scanner;
-
 public class Main {
-    int x = 50;
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        System.out.println("Enter here : ");
-        String str = in.next();
-        System.out.println(str);
-
-        Main mainobj = new Main();
-        System.out.println("The older value is : " + mainobj.x);
-        mainobj.x=1203;
-        System.out.println("The updated value is : " + mainobj.x);
+        System.out.println("Main Class!");
     }
 }
