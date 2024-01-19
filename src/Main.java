1abcc53c71eba618e25ade7dcb707fbeb8995be4
Dhruv <ddrathod510@gmail.com>
2024-01-18 21:24:23 -0500

    OOP conecept-New class added

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
-import java.util.Scanner;
+//import java.util.Scanner;
 public class Main {
+    int x = 2523;
     public static void main(String[] args) {
         System.out.println("This is Dhruv");
+        Dhruv myobj = new Dhruv();
+        System.out.println(myobj.age);
     }
 }
