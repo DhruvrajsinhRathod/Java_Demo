fb320e1219ece3274cbff148534890298bdc4e55
Dhruv <ddrathod510@gmail.com>
2024-01-18 21:20:25 -0500

    OOP conecept-New class added

---
+++ src/Dhruv.java
@@ -0,0 +1,9 @@
+public class Dhruv {
+    String str = "Passing this inside.";
+    int age = 25;
+    public static void main(String[] args) {
+        Dhruv obj = new Dhruv();
+        System.out.println("Message was : " + obj.str);
+        System.out.println("Age is : " + obj.age);
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,6 @@
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        System.out.print("Input seconds : ");
-        int seconds = in.nextInt();
-        int S = seconds % 60;  // Calculate the remaining seconds
-        int H = seconds / 60;  // Convert total seconds to minutes
-        int M = H % 60;         // Calculate the remaining minutes
-        H = H / 60;            // Convert total minutes to hours
-        System.out.print(H + ":" + M + ":" + S);
+        System.out.println("This is Dhruv");
     }
 }
