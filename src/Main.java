1d697d7cec6baeeea6092480125c96fc4546a894
Dhruv <ddrathod510@gmail.com>
2024-01-18 20:43:06 -0500

    odd numbers finder

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,13 @@
 import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.print("Input the string : ");
-        char[] letters = in.nextLine().toCharArray();
-
-        System.out.println("Reversed string : ");
-        for(int i=letters.length-1;i>=0;i--){
-            System.out.print(letters[i]);
+        int n = in.nextInt();
+        for(int i = 1;i<=n;i++){
+            if(i%2!=0){
+                System.out.println(i);
+            }
         }
     }
 }
