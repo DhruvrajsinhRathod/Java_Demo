d133c991db1da17a5794eb16022c65931f7ce173
Dhruv <ddrathod510@gmail.com>
2024-01-18 21:00:41 -0500

    calculate the sum of two integers and return true if the sum is equal to a third integer

--- src/Main.java
+++ src/Main.java
@@ -2,26 +2,18 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        System.out.print("Input the number : ");
-        int n = in.nextInt();
+        System.out.print("Input first : ");
+        int num1 = in.nextInt();
+        System.out.print("Input second : ");
+        int num2 = in.nextInt();
+        System.out.print("Input third : ");
+        int num3 = in.nextInt();
 
-        System.out.println("Divided by 3 : ");
-        for(int i=1;i<=n;i++){
-            if(i%3==0){
-                System.out.println(i + ", ");
-            }
+        if(num1+num2==num3){
+            System.out.println("True");
         }
-        System.out.println("Divided by 5 : ");
-        for(int i=1;i<=n;i++){
-            if(i%5==0){
-                System.out.println(i + ", ");
-            }
-        }
-        System.out.println("Divided by 3 & 5 both : ");
-        for(int i=1;i<=n;i++){
-            if(i%3==0 && i%5==0){
-                System.out.println(i + ", ");
-            }
+        else{
+            System.out.println("False");
         }
     }
 }
