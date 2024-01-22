a5e7d33d836627625497ecdcdb081939a21c32e2
Dhruv <ddrathod510@gmail.com>
2024-01-22 16:06:35 -0500

    Largest number method

--- src/Dhruv.java
+++ src/Dhruv.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
+
 public class Dhruv {
     public static void main(String[] args) {
-        System.out.println("Dhruv Class!");
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter The First Number : ");
+        int one = in.nextInt();
+        System.out.println("Enter The Second Number : ");
+        int two = in.nextInt();
+        System.out.println("Enter The Third Number : ");
+        int three = in.nextInt();
+        System.out.println("The largest number is : " + Smallest(one, two, three));
+    }
+
+    public static int Smallest(int one, int two, int three) {
+        int large = one;
+        if (two > large) {
+            large = two;
+        }
+        if(three > large){
+            large = three;
+        }
+        return large;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Main Class!");
+//        https://www.w3resource.com/java-exercises/basic/index.php from 63
     }
 }
